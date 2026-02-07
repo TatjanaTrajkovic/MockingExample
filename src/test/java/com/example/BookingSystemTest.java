429ae35f02f4bd9739056f30737bd1bd10400d2d
Tatjana <malina91983@gmail.com>
2026-02-07 23:11:30 +0100

    Create first test in BookingSystemTest class to book room succesfully

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class BookingSystemTest {
 
     private TimeProvider timeProvider;
     private RoomRepository roomRepository;
     private NotificationService notificationService;
+
     private BookingSystem bookingSystem;
 
 
@@ -22,6 +25,7 @@ class BookingSystemTest {
         timeProvider = mock(TimeProvider.class);
         roomRepository = mock(RoomRepository.class);
         notificationService = mock(NotificationService.class);
+
         bookingSystem = new BookingSystem(
                 timeProvider,
                 roomRepository,
@@ -34,6 +38,29 @@ class BookingSystemTest {
     void bookRoomSucessfully() throws Exception{
         //arange
         String roomId = "1";
-        LocalDateTime currentTime = LocalDateTime.of(2026, 2, 7, 22, 46);
+        LocalDateTime currentTime = LocalDateTime.of(2026, 2, 7, 22, 00);
+        when(timeProvider.getCurrentTime()).thenReturn(currentTime);
+
+        LocalDateTime startTime = currentTime.plusHours(1);
+        LocalDateTime endTime = currentTime.plusHours(2);
+
+        Room room = mock(Room.class);
+        when(roomRepository.findById("1")).thenReturn(Optional.of(room));
+        when(room.isAvailable(startTime, endTime)).thenReturn(true);
+
+        //act
+        boolean result = bookingSystem.bookRoom(roomId, startTime, endTime);
+
+        //assert
+        assertThat(result).isTrue();
+
+//        verify(room).addBooking(any(Booking.class));
+//        verify(roomRepository).save(room);
+//        verify(notificationService)
+//                .sendBookingConfirmation(any(Booking.class));
+
+
+
+
     }
 }

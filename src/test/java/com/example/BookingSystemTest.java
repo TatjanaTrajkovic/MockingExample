06d535532799d77d9e4e8f11a59e55d05c1ca05c
Tatjana <malina91983@gmail.com>
2026-02-07 22:48:13 +0100

    Create first test in BookingSystemTest

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -1,6 +1,10 @@
 package com.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -23,7 +27,13 @@ class BookingSystemTest {
                 roomRepository,
                 notificationService
         );
-
     }
 
+    @Test
+    @DisplayName("Should book room when romm exists and is available")
+    void bookRoomSucessfully() throws Exception{
+        //arange
+        String roomId = "1";
+        LocalDateTime currentTime = LocalDateTime.of(2026, 2, 7, 22, 46);
+    }
 }
